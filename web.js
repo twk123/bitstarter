86804f86a234a29c1dfa1d7bf5c30fb7d1bfc803
twk123 <tad@twkellogg.com>
2013-07-08 21:09:28 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   
-  var buf = fs.readFileSync('/index.html'); 
+  var buf = fs.readFileSync('index.html'); 
   response.send(buf.toString());
 });
 
