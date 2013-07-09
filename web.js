e4fee9f060b05cbcb47d29faf516beabdbed0a5f
twk123 <tad@twkellogg.com>
2013-07-08 20:59:43 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  fs.readFile('index.html',function (err, data) {
+	if (err) throw err;
+  	response.send(data);
+  }
 });
 
 var port = process.env.PORT || 5000;
